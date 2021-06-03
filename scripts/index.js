bf2488cd4dc58118c3b8d18431c2e3055efd4d1f
brewchetta <chett.tiller@gmail.com>
2021-06-03 15:04:10 -0400

    added various bugs

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,19 @@
 console.log('Bugs List:', bugs);
 
-const bugsList = document.querySelector('#bugs-list')
+const bugsList = document.querySelector('bugs-list')
 
-function appendBugToList(bug) {
+bugs.forEach(appendBugToList(bug));
+
+const appendBugToList = bug => {
   const li = document.createElement('li')
   const h3 = document.createElement('h3')
   const img = document.createElement('img')
 
-  h3.innerText = bug.name
-  img.src = bug.url
+  li.innerText = bug
+  h3.innerText = bug
+  img.src = bug
 
   li.append(h3)
   li.append(img)
   bugsList.append(li)
 }
-
-bugs.forEach(appendBugToList);
